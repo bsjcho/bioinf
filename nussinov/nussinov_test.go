09bf2fb0c27cf24f68fc6b88e382f2598d93e25c
Leo Cho <bsjcho@gmail.com>
2017-03-19 20:34:30 -0700

    nussinov benchmarks

--- nussinov/nussinov_test.go
+++ nussinov/nussinov_test.go
@@ -3,8 +3,9 @@ package nussinov
 import "testing"
 
 const (
-	checkMark = "\u2713"
-	ballotX   = "\u2717"
+	mIR1976   = "GCAGCAAGGAAGGCAGGGGTCCTAAGGTGTGTCCTCCTGCCCTCCTTGCTGT"
+	rNA5SP136 = "GTCTGTGGCCATACCACCCAGAACGCACTCGATCTCATCTTATCTCCAAAGCTAAGCATGGTTGGCCTGGTTAGTACTTGGATGGGAGAAAACTGTTATCCTACCTT"
+	bCYRN1    = "GGCCGGGCGCGGTGGCTCACGCCTGTAATCCCAGCTCTCAGGGAGGCTAAGAGGCGGGAGGATAGCTTGAGCCCAGGAGTTCGAGACCTGCCTGGGCAATATAGCGAGACCCCGTTCTCCAGAAAAAGGAAAAAAAAAAACAAAAGACAAAAAAAAAATAAGCGTAACTTCCCTCAAAGCAACAACCCCCCCCCCCCTTT"
 )
 
 func TestSolve(t *testing.T) {
@@ -12,9 +13,27 @@ func TestSolve(t *testing.T) {
 		t.Skip("skipping test in short mode.")
 	}
 	seq := "GGGAAAUCC"
-	t.Logf("When consiering the sequence %v\n", seq)
+	t.Logf("When considering the sequence %v\n", seq)
 	pairCount := Solve(seq)
 	if pairCount == 3 {
-		t.Logf("\tShould be able to calculate max base pairs %v\n", checkMark)
+		t.Logf("\tShould be able to calculate max base pairs\n")
+	}
+}
+
+func BenchmarkMIR1976(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Solve(mIR1976)
+	}
+}
+
+func BenchmarkRNA5SP136(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Solve(rNA5SP136)
+	}
+}
+
+func BenchmarkBCYRN1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Solve(bCYRN1)
 	}
 }
