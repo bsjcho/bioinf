51da75b0c8190b7e6e300bb7e239a8ed783fe0da
Leo Cho <bsjcho@gmail.com>
2017-03-23 03:32:06 -0700

    mdp almost working

--- msa/mdp/mdp.go
+++ msa/mdp/mdp.go
@@ -1,6 +1,7 @@
 package mdp
 
 import (
+	"fmt"
 	"math"
 
 	"github.com/bsjcho/nd"
@@ -25,19 +26,53 @@ const (
 	C
 	G
 	T
+	X // represents a gap "-"
 )
 
+func convertStringSequence(seq string) *Sequence {
+	s := NewSequence()
+	for _, b := range seq {
+		s.bases = append(s.bases, convertStringBase(string(b)))
+	}
+	return s
+}
+
+func convertStringBase(b string) Base {
+	switch b {
+	case "A":
+		return A
+	case "C":
+		return C
+	case "G":
+		return G
+	case "T":
+		return T
+	default:
+		return X
+	}
+}
+
 type multiDP struct {
-	seqs  []*Sequence
-	table *nd.Array
+	seqs        []*Sequence
+	table       *nd.Array
+	subsetMasks [][]int
 }
 
 func newMultiDP(s []*Sequence) *multiDP {
 
 	return &multiDP{
-		seqs:  s,
-		table: nd.NewArray(sizes(s)),
+		seqs:        s,
+		table:       nd.NewArray(sizes(s)),
+		subsetMasks: generateSubsetMasks(len(s)),
 	}
+}
+
+func generateSubsetMasks(size int) [][]int {
+	x := []int{}
+	for i := 0; i < size; i++ {
+		x = append(x, 1)
+	}
+	return findSubsets(x)
 }
 
 func sizes(s []*Sequence) (sizes []int) {
@@ -48,29 +83,155 @@ func sizes(s []*Sequence) (sizes []int) {
 }
 
 // Solve takes in a list of sequences and returns score of the optimal alignment
-func Solve(s []*Sequence) int {
+func Solve(s []*Sequence) float64 {
 	mdp := newMultiDP(s)
 	return mdp.solve()
 }
 
-func (m *multiDP) solve() int {
-	m.initialize()
-	m.fillTables()
-	return m.table.At(m.maxIndices())
+func (m *multiDP) solve() float64 {
+	maxIdxs := m.maxIndices()
+	fmt.Println(maxIdxs)
+	return float64(m.f(maxIdxs)) / 2
 }
 
-func (m *multiDP) initialize() {
-	// TODO
+func (m *multiDP) f(idxs []int) (best int) {
+	for _, i := range idxs {
+		if i < 0 {
+			return
+		}
+	}
+	if m.table.At(idxs) != 0 {
+		fmt.Printf("cached f(%v): %v\n", idxs, m.table.At(idxs))
+		return m.table.At(idxs)
+	}
+	for _, mask := range m.subsetMasks {
+		mIdxs, ok := maskedIdxs(idxs, mask)
+		fmt.Printf("mIdxs f(%v): %v - %v\n", idxs, mIdxs, ok)
+		if !ok { // if an idx is below 0, don't consider this option
+			continue
+		}
+		bestf := m.f(mIdxs)
+		score := m.score(m.maskedBases(idxs, mask))
+		fmt.Printf("score f(%v): %v\n", idxs, score)
+		best = max(best, bestf+score)
+	}
+	m.table.Set(best, idxs)
+	fmt.Printf("calced f(%v): %v\n", idxs, best)
+	return
 }
 
-func (m *multiDP) fillTables() {
-	// TODO
+func maskedIdxs(idxs, mask []int) (mIdxs []int, ok bool) {
+	for i, idx := range idxs {
+		x := idx - mask[i]
+		if x < 0 {
+			return mIdxs, false
+		}
+		mIdxs = append(mIdxs, x)
+	}
+	return mIdxs, true
+}
+
+func (m *multiDP) maskedBases(idxs, mask []int) (bases []Base) {
+	for i, idx := range idxs {
+		var b Base
+		if mask[i] == 1 { // not a gap
+			b = m.seqs[i].bases[idx-1]
+		} else {
+			b = X
+		}
+		bases = append(bases, b)
+	}
+	return
 }
 
 func (m *multiDP) maxIndices() (indices []int) {
 	for _, size := range sizes(m.seqs) {
 		indices = append(indices, size-1)
 	}
+	return
+}
+
+func (m *multiDP) score(bases []Base) (sum int) {
+	for i, bi := range bases[:len(bases)-1] {
+		for _, bj := range bases[i+1:] {
+			sum += pairScore(bi, bj)
+		}
+	}
+	return
+}
+
+const (
+	// values doubled to be able to use integers during calculations
+	match    = 6
+	mismatch = -4
+	gap      = -3
+)
+
+func pairScore(b1, b2 Base) int {
+	if b1 == X && b2 == X {
+		return 0
+	}
+	if (b1 == X && b2 != X) ||
+		(b2 == X && b1 != X) {
+		return gap
+	}
+	if b1 != b2 {
+		return mismatch
+	}
+	return match
+}
+
+///////////////////////// Helpers
+
+func subbedIndices(idxs []int) (si []int) {
+	for _, i := range idxs {
+		si = append(si, i-1)
+	}
+	return
+}
+
+func findSubsets(idxs []int) (subsets [][]int) {
+	subsets = subsetHelper(idxs, len(idxs)-1)
+	return purgeAllGapCase(idxs, subsets)
+}
+
+func purgeAllGapCase(idxs []int, subsets [][]int) (ss [][]int) {
+	si := subbedIndices(idxs)
+	for _, subset := range subsets {
+		if !arraysMatch(subset, si) {
+			ss = append(ss, subset)
+		}
+	}
+	return
+}
+
+func arraysMatch(s1, s2 []int) bool {
+	var count int
+	for i, v := range s1 {
+		if s2[i] == v {
+			count++
+		}
+	}
+	return count == len(s1)
+}
+
+func subsetHelper(idxs []int, i int) (subsets [][]int) {
+	if i == -1 {
+		return append(subsets, []int{})
+	}
+	x := idxs[i]
+	for _, subset := range subsetHelper(idxs, i-1) {
+		s1 := cpy(subset)
+		s2 := cpy(subset)
+		subsets = append(subsets, append(s1, x))
+		subsets = append(subsets, append(s2, x-1))
+	}
+	return
+}
+
+func cpy(a []int) (b []int) {
+	b = make([]int, len(a))
+	copy(b, a)
 	return
 }
 
--- msa/mdp/mdp_test.go
+++ msa/mdp/mdp_test.go
@@ -1,14 +1,65 @@
 package mdp
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 const (
 	x1 = "AATTATGG"
 	x2 = "ACATTGTTG"
 	x3 = "GCCAGGAGG"
 	x4 = "AATTTTGAGG"
+
+	x5 = "AA"
+	x6 = "AA"
+	x7 = "AA"
+	x8 = "AA"
 )
 
-func TestMDP(t *testing.T) {
+func solveSeqs(seqStrings []string) float64 {
+	seqs := []*Sequence{}
+	for _, seqStr := range seqStrings {
+		seqs = append(seqs, convertStringSequence(seqStr))
+	}
+	mdp := newMultiDP(seqs)
+	return mdp.solve()
+}
 
+func TestMDP(t *testing.T) {
+	// fmt.Println(solveSeqs([]string{x1, x2, x3, x4}))
+}
+
+func TestMDPSimple(t *testing.T) {
+	optScore := solveSeqs([]string{x5, x6, x7})
+	fmt.Println(optScore)
+	if optScore != 36 {
+		t.Error("wrong score for simple MDP")
+	}
+}
+
+func TestConversion(t *testing.T) {
+	seq := convertStringSequence(x1)
+	rSeq := []Base{A, A, T, T, A, T, G, G}
+	for i, b := range seq.bases {
+		if rSeq[i] != b {
+			t.Errorf("base conversion failed: %v %v\n", rSeq[i], b)
+		}
+	}
+}
+
+func TestFindSubsets(t *testing.T) {
+	idxs := []int{1, 1, 1}
+	ss := findSubsets(idxs)
+	fmt.Println(len(ss))
+	for _, z := range ss {
+		fmt.Println(z)
+	}
+}
+
+func TestGenSubMasks(t *testing.T) {
+	ss := generateSubsetMasks(4)
+	for _, z := range ss {
+		fmt.Println(z)
+	}
 }
